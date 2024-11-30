e5d931b01726a2274902f6fc92e5b2ec076fb186
Iambnthach <iambnthach@gmail.com>
2024-11-30 21:41:15 +0700

    add port

--- index.js
+++ index.js
@@ -3,16 +3,15 @@ const app = express();
 const pug = require("pug");
 const mongoose = require("mongoose");
 require("dotenv").config();
-const database
-    = require("./config/database");
+const database = require("./config/database");
 const router = require("./routes/index.router");
 
-app.set("views", "views");
+const port = process.env.PORT || 3000;
+app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
-app.use(express.static("public"));
+app.use(express.static(`${__dirname}/public`));
 
 database.connect();
-
 router(app);
 app.listen(3000);
 console.log("http://localhost:" + port);
